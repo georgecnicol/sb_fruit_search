b3483852c7b6b1cf0c888203edfbf69dbb4f1076
George Nicol <georgecnicol@gmail.com>
2023-09-21 01:07:28 -0700

    refactor input to not focus on key stroke, but rather input field. tweak css more.

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions');
-const notLetters = ['CapsLock', 'Shift', 'ArrowUp', 'Enter', 'ArrowDown', 'ArrowRight', 'ArrowLeft', 'Escape', 'Backspace', 'Meta', 'Option', 'Alt' ]
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
-let typedFruit = '';
 
 
 function search(str) {
 	let results = [];
 	if (str){
+		str = str.toLowerCase();
 		results = fruit.filter(pieceOfFruit => pieceOfFruit.toLowerCase().includes(str));
 	}
 	return results;
 }
 
-function processInput(theKey){
-	theKey = theKey.toLowerCase();
-	let typeOfKey = notLetters.filter(notLetter => notLetter.toLowerCase() === theKey);
-
-	if (typeOfKey.length > 0){
-		if (theKey === 'backspace' && typedFruit.length > 0){
-			typedFruit = typedFruit.slice(0,-1);
-		}
-	} else { // regular letter or number or similar usuable input
-		typedFruit += theKey;
-	}
-
-	let fruitList = search(typedFruit);
-	showSuggestions(fruitList, 9);
-
-	return fruitList; // return value used in testing only
-
-}
-
-
 // as user is typing get type ahead matches
 function searchHandler(e) {
-	processInput(e.key);
-
+	showSuggestions(search(e.target.value));
+	// originally used e.key and processed keystrokes only rather than value in the field
+	// now the need to deal with special keystrokes is gone.
 }
 
 function removeList(){
@@ -47,36 +27,25 @@ function removeList(){
 
 function addListItem(text){
 	const newLI = document.createElement('li');
-	const newButton = document.createElement('button');
-	newButton.setAttribute('class', 'result-button')
-	newButton.innerText = text;
-	newLI.append(newButton);
+	newLI.setAttribute('class', 'result-li');
+	newLI.innerText = text;
 	suggestions.append(newLI);
 }
 
 
 // display type ahead matches
-function showSuggestions(results, inputVal) {
-	// destroy old list
+function showSuggestions(results) { // what was input val?
 	removeList();
 	// make new list
 	for (let item of results){
 		addListItem(item);
 	}
-
-
 }
 
 // when user clicks on suggestion
 function useSuggestion(e) {
-	e.preventDefault();
-	if (e.target.tagName === 'BUTTON'){
-		let inputText = document.getElementById('fruit');
-		inputText.value = e.target.innerText;
-		removeList();
-		typedFruit = '';
-	}
-
+	input.value = e.target.innerText;
+	removeList();
 }
 
 input.addEventListener('keyup', searchHandler);
--- script.test.js
+++ script.test.js
@@ -1,48 +1,32 @@
 
-describe("Process Input Test", function() {
+describe("Search Test", function() {
     beforeEach(function () {
     // initialization logic
-        typedFruit = '';
     });
 
     afterEach(function () {
         // initialization logic
-            typedFruit = '';
             removeList();
         });
 
-    it('should handle shift, arrows, capslock, option, meta as keystroke in empty search field', function () {
-        for (let item of notLetters){
-            expect(processInput(item).length).toBe(0);
-            typedFruit = '';
-        }
-    });
-
-    it('should handle shift, arrows, capslock, option, meta as keystroke in non-empty field', function() {
-        for (let item of notLetters){
-            typedFruit = 'banana';
-            expect(processInput(item).length).toBe(1); // Banana
-            expect(processInput(item)[0]).toEqual('Banana');
-            typedFruit = '';
-        }
-    });
-
     it('should handle capitalized input', function(){
-        expect(processInput('BANaNA').length).toBe(1); // Banana
-        typedFruit = '';
-        expect(processInput('BANaNA')[0]).toEqual('Banana');
+        expect(search('BANaNA').length).toBe(1); // Banana
+        expect(search('BANaNA')[0]).toEqual('Banana');
     });
 
     it('should not find banananana', function(){
-        expect(processInput('banananana').length).toBe(0); // Banana
+        expect(search('banananana').length).toBe(0); // Banana
     });
 
     it('should find partial matches', function(){
-        expect(processInput('rran').length).toBe(3);
-        typedFruit = '';
-        expect(processInput('rran')[1]).toEqual('Currant');
-        typedFruit = '';
+        expect(search('rran').length).toBe(3);
+        expect(search('rran')[1]).toEqual('Currant');
     });
 
+    it('should make line items', function(){
+        addListItem('blueberry');
+        addListItem('Strawberry');
+        expect(suggestions.childElementCount).toBe(2);
+    });
 
 });
